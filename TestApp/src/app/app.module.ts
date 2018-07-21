7349edd3193d4e0025519f875c56565eabfa2d8d
Yogesh Rajput <yogeshrajput335@gmail.com>
2018-07-21 21:23:16 +0530

    leftsidepage

--- TestApp/src/app/app.module.ts
+++ TestApp/src/app/app.module.ts
@@ -3,11 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { LeftsideComponent } from './leftside/leftside.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    LeftsideComponent,
+    
   ],
   imports: [
     BrowserModule
