42454403e64aec0ad647ef1bbabff405c7f37a72
akash <aka01sh@gmail.com>
2018-07-22 20:04:04 +0530

    add leftside

--- TestApp/src/app/leftside/leftside.component.ts
+++ TestApp/src/app/leftside/leftside.component.ts
@@ -7,8 +7,82 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LeftsideComponent implements OnInit {
 
-  constructor() { }
+  showSelected : boolean;
+  showSelectedAbout:boolean;
+  showSelectedLike:boolean;
+  showSelectedAboutUs:boolean;
+  showSelectedSupport:boolean;
+  showSelectedBlog:boolean;
+  showSelectedTools:boolean;
 
+  constructor(){
+      this.showSelected = false;     
+  }
+  ShowButton(){
+      this.showSelected = true;
+      this.showSelectedAbout = false;
+      this.showSelectedAboutUs = false;
+      this.showSelectedLike=false;
+      this.showSelectedSupport=false;
+      this.showSelectedBlog = false;
+     this. showSelectedTools= false;
+  }
+  HideButton(){
+    this.showSelected = false;
+    this.showSelectedAbout = true;
+    this.showSelectedAboutUs = false;
+    this.showSelectedLike=false;
+    this.showSelectedSupport=false;
+    this.showSelectedBlog = false;
+    this. showSelectedTools= false;
+  }
+  LikeButton(){
+    this.showSelected = false;
+    this.showSelectedAbout = false;
+    this.showSelectedAboutUs = false;
+    this.showSelectedLike=true;
+    this.showSelectedSupport=false;
+    this.showSelectedBlog = false;
+    this. showSelectedTools= false;
+    }
+  SetButton(){
+    this.showSelected = false;
+    this.showSelectedAbout = false;
+    this.showSelectedAboutUs = true;
+    this.showSelectedLike=false;
+    this.showSelectedSupport=false;
+    this.showSelectedBlog = false;
+    this. showSelectedTools= false;
+    }
+   supportButton(){
+    this.showSelected = false;
+    this.showSelectedAbout = false;
+    this.showSelectedAboutUs =false;
+    this.showSelectedLike=false;
+    this.showSelectedSupport=true;
+    this.showSelectedBlog = false;
+    this. showSelectedTools= false;
+    }
+
+    BlogButton(){
+      this.showSelected = false;
+      this.showSelectedAbout = false;
+      this.showSelectedAboutUs =false;
+      this.showSelectedLike=false;
+      this.showSelectedSupport=false;
+      this.showSelectedBlog = true;
+      this. showSelectedTools= false;
+      }
+      ToolsButton(){
+        this.showSelected = false;
+        this.showSelectedAbout = false;
+        this.showSelectedAboutUs =false;
+        this.showSelectedLike=false;
+        this.showSelectedSupport=false;
+        this.showSelectedBlog = false;
+        this. showSelectedTools= true;
+        }
+  
   ngOnInit() {
   }
 
